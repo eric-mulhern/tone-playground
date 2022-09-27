c6ab6c2bf072a2ad86efd771279b0d895f338885
Eric Mulhern <eric.mulhern@gmail.com>
2022-09-26 19:39:29 -0700

    added set multiplyer button

--- components/playground.tsx
+++ components/playground.tsx
@@ -8,6 +8,9 @@ const Playground = () => {
   const [isLoaded, setLoaded] = useState(false);
   const synth: any = useRef<Tone>(null);
 
+  const [size, setSize] = useState(50);
+  const [multiplyer, setMultiplyer] = useState(3);
+
   useEffect(() => {
     setLoaded(true);
     synth.current = new tone.Synth().toDestination();
@@ -23,24 +26,24 @@ const Playground = () => {
   const cMajor = ["C", "D", "E", "F", "G", "A", "B"];
   const itNeverWasAMinor = [100, 200, 300, 400, 500, 600, 700];
 
-  const SIZE = 30;
+
   let columns: Array<ReactElement> = [];
-  for (let i = 1; i <= SIZE; i++) {
+  for (let i = 1; i <= size; i++) {
     const row: any[] = [];
-    for (let j = 1; j <= SIZE; j++) {
+    for (let j = 1; j <= size; j++) {
       row.push(
         <button
         disabled={!isLoaded}
         onClick={() => {
-          playNote(i * j * 10, "8n", 1.5);
+          playNote(i * j * multiplyer, "8n", 1.5);
         }}
         onMouseOver={() => {
-          playNote(i * j * 10, "8n", 1);
+          playNote(i * j * multiplyer, "8n", 1);
         }}
-        key={i * j * 10 + i + j}
+        key={i * j * multiplyer + i + j}
         className={styles.gridItem}
       >
-        {i * j * 10}
+        {i * j * multiplyer}
       </button>
       );
     }
@@ -55,6 +58,16 @@ const Playground = () => {
     <div>
       <div className={styles.gridContainer}>
         { columns }
+      </div>
+      <div style={{ width: 200, height: 100, marginTop: 50}}>
+        SET MULTIPLYER
+        <br />
+        <button style={{ width: 90, height: 40}} onClick={() => {setMultiplyer(10)}}>
+          10
+        </button>
+        <button style={{ width: 90, height: 40, marginTop: 50}} onClick={() => {setMultiplyer(2)}}>
+          2
+        </button>
       </div>
     </div>
   );
